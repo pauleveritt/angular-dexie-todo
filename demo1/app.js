d19a5e5bc0f41eb372ad5cfd7b7f73e2f69aef8b
Paul Everitt <paul@agendaless.com>
2015-01-24 12:00:49 -0500

    Working non-Angular version.

---
+++ demo1/app.js
@@ -0,0 +1,46 @@
+(function () {
+  angular.module('app', []);
+  var db = new Dexie("todos-dexie");
+  var input = document.querySelector('input');
+  var ul = document.querySelector('ul');
+  document.body.addEventListener('submit', onSubmit);
+  document.body.addEventListener('click', onClick);
+
+  db.version(1).stores({todo: '_id'})
+  db.open()
+    .then(refreshView);
+
+  function onClick(e) {
+    e.preventDefault();
+    if (e.target.hasAttribute('id')) {
+      db.todo.where('_id').equals(e.target.getAttribute('id')).delete()
+        .then(refreshView);
+    }
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    db.todo.put({text: input.value, _id: String(Date.now())})
+      .then(function () {
+              input.value = '';
+            })
+      .then(refreshView);
+  }
+
+  function refreshView() {
+    return db.todo.toArray()
+      .then(renderAllTodos);
+  }
+
+  function renderAllTodos(todos) {
+    var html = '';
+    todos.forEach(function (todo) {
+      html += todoToHtml(todo);
+    });
+    ul.innerHTML = html;
+  }
+
+  function todoToHtml(todo) {
+    return '<li><button id="' + todo._id + '">delete</button>' + todo.text + '</li>';
+  }
+}());
