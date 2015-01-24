316f8b4b106c57d91144ccf1f947cbc9a35ced88
Paul Everitt <paul@agendaless.com>
2015-01-24 13:25:36 -0500

    Listening works.

--- demo1/app.js
+++ demo1/app.js
@@ -1,73 +1,53 @@
 (function () {
-  var db = new Dexie("todos-dexie");
-  var input = document.querySelector('input');
-  var ul = document.querySelector('ul');
-  document.body.addEventListener('submit', onSubmit);
-  document.body.addEventListener('click', onClick);
-
+  var db = new Dexie('todos');
   db.version(1).stores({todo: '_id'});
-  db.open()
-    .then(refreshView);
+  db.open();
 
-  function onClick(e) {
-    e.preventDefault();
-    if (e.target.hasAttribute('id')) {
-      db.todo.where('_id').equals(e.target.getAttribute('id')).delete()
-        .then(refreshView);
-    }
-  }
-
-  function onSubmit(e) {
-    e.preventDefault();
-    db.todo.put({text: input.value, _id: String(Date.now())})
-      .then(function () {
-              input.value = '';
-            })
-      .then(refreshView);
-  }
-
-  function refreshView() {
-    return db.todo.toArray()
-      .then(renderAllTodos);
-  }
-
-  function renderAllTodos(todos) {
-    var html = '';
-    todos.forEach(function (todo) {
-      html += todoToHtml(todo);
-    });
-    ul.innerHTML = html;
-  }
-
-  function todoToHtml(todo) {
-    return '<li><button id="' + todo._id + '">delete</button>' + todo.text + '</li>';
-  }
-
-  function AppController($scope) {
+  function AppController($scope, $window) {
     var ctrl = this;
     ctrl.todos = [];
     ctrl.newTitle = '';
-    db.todo.toArray(function (todos) {
-      $scope.$apply(function () {
-        ctrl.todos = todos;
-      })
-    });
+
+    function refresh() {
+      db.todo.toArray(function (todos) {
+        $scope.$apply(function () {
+          ctrl.todos = todos;
+        })
+      });
+    }
+
+    refresh();
 
     ctrl.addToDo = function (newTitle) {
-      $scope.$apply(function () {
-        db.todo.put({text: newTitle, _id: String(Date.now())})
-          .then(function () {
-                  ctrl.newTitle = '';
-                });
-      });
+      console.log('adding', newTitle)
+      db.todo.put({text: newTitle, _id: String(Date.now())})
+        .then(function () {
+                ctrl.newTitle = '';
+              });
     };
 
     ctrl.deleteToDo = function (id) {
-      $scope.$apply(function () {
-        db.todo.where('_id').equals(id).delete()
-          .then(refreshView);
-      });
+      db.todo.where('_id').equals(id).delete();
     };
+
+    $window.db = db;
+
+    // Listeners
+    db.todo.hook('deleting', function (primKey, obj, transaction) {
+      this.onsuccess = function () {
+        refresh();
+      }
+    });
+    db.todo.hook('creating', function (primKey, obj, transaction) {
+      this.onsuccess = function () {
+        refresh();
+      }
+    });
+    db.todo.hook('updating', function (primKey, obj, transaction) {
+      this.onsuccess = function () {
+        refresh();
+      }
+    });
   }
 
   angular.module('app', [])
